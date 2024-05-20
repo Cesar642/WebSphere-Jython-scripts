59fb681614281d75ecf09e82b73b7c2e3145152f
Cesar642 <44422221+Cesar642@users.noreply.github.com>
2024-05-20 16:40:54 -0700

    Create deleteEntrySpecific.py

---
+++ scripts/deleteEntrySpecific.py
@@ -0,0 +1,63 @@
+import sys
+
+fileName = sys.argv[0]
+whatToDelete = sys.argv[1]
+
+nameDic={}
+#fp = open("NamesInBinding.out")
+fp = open(fileName)
+
+def createdic(fileRef):
+  try:
+    for line in fileRef:
+      #print line,
+      lineSplit = line.split('\n')[0]
+      nameDic[lineSplit]=lineSplit
+  finally:
+    fileRef.close()
+
+def printDic():
+  print nameDic
+
+def findKey(key):
+  exist = False
+  if key in nameDic:
+    print "key %s found with value %s" % (key, nameDic[key])
+    exist = True
+  return exist
+
+def saveConfig():
+  print "Saving..."
+  AdminConfig.save()
+  print "All changes saved"
+  syncNodes()
+
+def syncNodes():
+  # Obtain deployment manager MBean
+  dm=AdminControl.queryNames("type=DeploymentManager,*")
+
+  # "syncActiveNodes" can only be run on the deployment manager's MBean,
+  # it will fail in standalone environment
+  if dm:
+    print "Synchronizing configuration repository with nodes. Please wait..."
+    # Force sync with all currently active nodes
+    nodes=AdminControl.invoke(dm, "syncActiveNodes", "true")
+    print "The following nodes have been synchronized: "+str(nodes)
+  else:
+    print "Standalone server, no nodes to sync"
+
+def deleteKey(toDelete):
+  for ns in AdminConfig.list( toDelete ).splitlines() :
+    #name = AdminConfig.showAttribute( ns, 'name' )
+    #if (findKey(name)):
+	if (findKey(ns)):
+	  print "Delete %s" % ns
+	  AdminConfig.remove(ns)
+
+createdic(fp)
+
+#printDic()
+
+deleteKey(whatToDelete)
+
+saveConfig()

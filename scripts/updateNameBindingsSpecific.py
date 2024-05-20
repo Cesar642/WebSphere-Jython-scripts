91797900747e1170315d2d5c46b6d3690c216c1a
Cesar642 <44422221+Cesar642@users.noreply.github.com>
2024-05-20 16:37:42 -0700

    Update updateNameBindingsSpecific.py

--- scripts/updateNameBindingsSpecific.py
+++ scripts/updateNameBindingsSpecific.py
@@ -74,8 +74,6 @@ def updateKeys(toUpdate):
       print "AdminConfig.modify(%s, %s)" % (typeToEdit, params)
       print AdminConfig.modify(typeToEdit, params)
 
-#findKey("HSBC/SECF/GLOBAL/CONFIGMANAGER/CONFIG/externalConfig")
-
 createdic(fp)
 
 #printDic()
